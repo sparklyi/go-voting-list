1c3fc9a2112edefe453c7029168f4896c9852896
li <513254687@qq.com>
2024-04-28 23:22:22 +0800

    use const value to config

--- dao/dao.go
+++ dao/dao.go
@@ -1,9 +1,8 @@
 package dao
 
 import (
-	"fmt"
+	"gin_Ranking/config"
 	"gin_Ranking/pkg/logger"
-	"gopkg.in/ini.v1"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -14,22 +13,9 @@ var (
 )
 
 func init() {
-	//todo 从ini文件中读取mysql相关内容
-	//加载my.ini文件
-	cnf, iniErr := ini.Load("config.ini")
-	if iniErr != nil {
-		logger.Error(map[string]interface{}{"read ini file failed": iniErr.Error()})
-	}
-	//读取mysql区块的数据
-	pwd := cnf.Section("mysql").Key("password").String()
-	address := cnf.Section("mysql").Key("address").String()
-	port := cnf.Section("mysql").Key("port").String()
-	db := cnf.Section("mysql").Key("db").String()
-
-	dsn := fmt.Sprintf("root:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", pwd, address, port, db)
-
+	//连接数据库
 	DB, err = gorm.Open(mysql.New(mysql.Config{
-		DSN: dsn,
+		DSN: config.MysqlDsn,
 	}), &gorm.Config{})
 
 	if err != nil {
