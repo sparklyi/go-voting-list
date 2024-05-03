3317b34c690172bc3987dd3ac598c5064cdebbca
sparkyi <64263737+sprakyi@users.noreply.github.com>
2024-05-03 22:07:37 +0800

    Update redis.go

--- config/redis.go
+++ config/redis.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	RedisAddr   = "redis-17243.c266.us-east-1-3.ec2.redns.redis-cloud.com:17243"
-	RedisPasswd = "ly513254687"
+	RedisAddr   = "localhost:6379"
+	RedisPasswd = "password"
 	RedisDB     = 0
 )
